42195c816ee7b8ed7260bb722ca276f80ba03de1
Jennifer Huestis <jenrhues@ut.utm.edu>
2020-04-11 17:21:25 -0500

    adding functions to GameEngine.cs

--- Game/GameEngine.cs
+++ Game/GameEngine.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Game.ItemCreatorFile;
+using System.Windows.Media.Imaging;
+using Game.ObstacleFactory;
+using Game.Eitities;
 
 namespace Game
 {
@@ -11,6 +14,26 @@ namespace Game
     class GameEngine
     {
         ItemCreator ItemSpawner = new ItemCreator();
+        ObstacleCreator ObstacleSpawner = new ObstacleCreator();
+        Player User = new Player();
+        // referencing the world in xaml.cs
+        // frame used in world.xaml.cs
+        public void Start()
+        {
+
+
+        }
+
+        public void TogglePause()
+        {
+
+        }
+
+        public void DeathEvents(WriteableBitmap screen)
+        {
+
+        }
+
         public void Update()
         {
 
