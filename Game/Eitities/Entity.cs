cb5758764c8b30cd4743022acd50f82e48f68650
Jennifer Huestis <jenrhues@ut.utm.edu>
2020-04-14 17:09:08 -0500

    Added incomplete code to Entity and Player

--- Game/Eitities/Entity.cs
+++ Game/Eitities/Entity.cs
@@ -3,14 +3,83 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using static System.Windows.Media.Imaging.WriteableBitmapExtensions;
+
+// TO DO: Is StageGraphics needed?
+//using Game.Data;
 
 namespace Game
 {
-    class Entity
+    public abstract class Entity
     {
-        public void Update()
-        {
+        // TO DO: depends on StageGraphics
+        //public int floor = (int)(StageGraphics.FloorPos.Y);
 
+        public int AnimationIndex = 0, speed;
+        public int FrameCount = 0, Fpa = 10; //fpa is frames per animation.
+        public double leftbound = 0, rightbound;
+
+        //stores all animations for an entity
+        public List<string> CurrentAnimation = null;
+        public List<string> previousAnimation = null;
+        public List<string> runAnimation = null;
+
+
+        // TO DO: figure out why Vector2 won't work?
+        public Vector2 Position { get; set; } //Location of game entity. = (X, Y).
+        public Vector2 Velocity { get; set; } //Velocity of game entity in pixels/sec. = (Change of X, Change of Y)
+
+        public Entity()
+        {
+            setSpeed();
+        }
+
+        public abstract void setSpeed();
+        public abstract void SetVelocity(); //sets the velocity for the next movement.
+        public abstract void LoadAnimations();
+        public abstract void setAnimation();
+
+        public Size GetSpriteSize()
+        {
+            if (CurrentAnimation != null)
+            {
+                return new Size((double)new BitmapImage(new Uri(CurrentAnimation[AnimationIndex], UriKind.Relative)).PixelWidth,
+                    (double)new BitmapImage(new Uri(CurrentAnimation[AnimationIndex], UriKind.Relative)).PixelHeight);
+            }
+            else return new Size(0, 0);
+        }
+
+        public virtual void Draw(WriteableBitmap surface)
+        {
+            BitmapImage img = new BitmapImage(new Uri(CurrentAnimation[AnimationIndex], UriKind.Relative));
+            WriteableBitmap bm = new WriteableBitmap(img);
+
+            //resets animation index to 0 if animation index + 1 is out of bounds
+            if (AnimationIndex >= CurrentAnimation.Count - 1)
+                AnimationIndex = 0;
+            //Increments animation index by 1 when FrameCount is less then fpa
+            else if ((FrameCount += 1) > Fpa)
+            {
+                AnimationIndex += 1;
+                FrameCount = 0;
+            }
+        }
+
+        public void Update(float millisecondsPassed)
+        {
+            //some code may not be necessary or may be used differently
+            /*
+            //set right bound for specific image
+            rightbound = StageGraphics.WindowWidth - GetSpriteSize().Width;
+            SetVelocity();
+            //Calculate what floor should be.
+            floor = (int)(StageGraphics.FloorPos.Y - GetSpriteSize().Height);
+            Position += Velocity * (millisecondsPassed / 1000f);
+            */
         }
     }
 }
--- Game/Eitities/Player.cs
+++ Game/Eitities/Player.cs
@@ -3,13 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Game.Eitities
 {
-    class Player
+    class Player : Entity
     {
+        bool jumping, falling, ducking;
+        int jumpForce = 400, force;
+        public List<string> jumpAnimation;
+        public List<string> duckAnimation;
+
+        // TO DO: Start() versus Player()?? 
+        public Player() : base()
+        {
+            LoadAnimations();
+            force = -jumpForce;
+
+            jumping = false; falling = false;
+            //Initial position
+            // TO DO: needs a floor in entity
+            Position = new System.Numerics.Vector2((float)(StageGraphics.WindowWidth / 2), floor -= (int)GetSpriteSize().Height);
+        }
+
         //starts the running animation and allows you to control player with arrow keys
         public void Start()
+        {
+
+        }
+
+        // TO DO: needed??
+        public override void setSpeed()
+        {
+            //speed = 240;
+        }
+
+        // TO DO: needed??
+        public override void SetVelocity()
         {
 
         }
@@ -17,17 +47,62 @@ namespace Game.Eitities
         //check for arrow keys pressed then jump or duck if correct key is pressed
         public void Update()
         {
+            if(Keyboard.IsKeyDown(Key.Down))
+            {
+                Duck();
+            }
+            if(Keyboard.IsKeyDown(Key.Up))
+            {
+                Jump();
+            }
+        }
+        // TO DO: should this be in Update()??
+        /*
+        public override void GameTick(float millisecondsPassed)
+        {
+            base.GameTick(millisecondsPassed);
+        }
+        */
 
+
+        // TO DO: Figure out algorithm for jumping in place. 
+        //Should just move the player up and down in a straight line. Should switch animation when player begins falling.
+        void Jump()
+        {
+            jumping = true;
+            setAnimation();
         }
 
-        void jump()
+        // TO DO: Change Octocat size to be shorter with the animation to avoid obstacles.
+        //Should scrunch up Octocat, but should not move Octocat.
+        void Duck()
         {
-
+            ducking = true;
+            setAnimation();
         }
 
-        void duck()
+        public override void LoadAnimations()
         {
+            CurrentAnimation = HelperClasses.EntityAnimations.OctocatRun;
+            runAnimation = HelperClasses.EntityAnimations.OctocatRun;
+            jumpAnimation = HelperClasses.EntityAnimations.OctocatJump;
+            duckAnimation = HelperClasses.EntityAnimations.OctocatDuck;
+        }
 
+        public override void setAnimation()
+        {
+            previousAnimation = CurrentAnimation;
+
+            if (ducking)
+            {
+                CurrentAnimation = duckAnimation;
+            }
+            else if (jumping && !falling)
+            {
+                CurrentAnimation = jumpAnimation;
+            }
+
+            if (previousAnimation != CurrentAnimation) AnimationIndex = 0;
         }
     }
 }
