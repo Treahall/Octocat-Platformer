5c9151fb9f955af97cf2e39b8fba1aaa462ed3b7
Jennifer Huestis <jenrhues@ut.utm.edu>
2020-04-23 06:47:28 -0500

    Item's inherited members have basic code

--- Game/ItemCreator/NyanCat.cs
+++ Game/ItemCreator/NyanCat.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Game.Entities;
 
 namespace Game.ItemCreatorFile
 {
     class NyanCat : Item
     {
+        public NyanCat(Player U, int speed) : base(U, speed)
+        {
+
+        }
+        public override void CollisionEvents()
+        {
+
+        }
+
+        public override void LoadAnimations()
+        {
+            CurrentAnimation = HelperClasses.ItemAnimations.NyanCat;
+        }
     }
 }
